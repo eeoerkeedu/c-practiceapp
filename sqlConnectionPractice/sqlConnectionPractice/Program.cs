62e5157341b116efd1b622c301ae7011fad013b4
LilTzar <eeoerkeedu@gmail.com>
2022-11-14 19:19:33 -0700

    added initial code for connection, also some dependancies

--- sqlConnectionPractice/sqlConnectionPractice/Program.cs
+++ sqlConnectionPractice/sqlConnectionPractice/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace sqlConnectionPractice
 {
@@ -10,6 +11,32 @@ namespace sqlConnectionPractice
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Getting Connection ...");
+            var datasource = @"localhost:3306\SQLEXPRESS"; //your server
+            var database = "students_db"; //your database name
+
+
+            //your connection string 
+            string connString = @"Data Source=" + datasource + ";Initial Catalog=" + database + ";Persist Security Info=True;User ID=" + username + ";Password=" + password;
+
+            //create instanace of database connection
+            SqlConnection conn = new SqlConnection(connString);
+
+            try
+            {
+                Console.WriteLine("Openning Connection ...");
+
+                //open connection
+                conn.Open();
+
+                Console.WriteLine("Connection successful!");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
+
+            Console.Read();
         }
     }
 }
